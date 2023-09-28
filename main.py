281044b2e51a278c62817649eee678014e6b8bb7
saldaandres <100974788+saldaandres@users.noreply.github.com>
2023-09-27 19:58:28 -0500

    Actualizacion para entrega

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ def crear_tabla(equipos):
         resultados = {
             'nombre': equipo,
             'puntos': 0,
-            'goles a favor': 0,
-            'goles en contra': 0
+            'GF': 0,
+            'GC': 0
         }
         tabla.append(resultados)
     return tabla
@@ -31,11 +31,11 @@ def actualizar_tabla(partido, tabla):
         if equipo['nombre'] == equipo2:
             equipo_dos = equipo
 
-    equipo_uno['goles a favor'] = equipo_uno['goles a favor'] + partido[1]
-    equipo_uno['goles en contra'] = equipo_uno['goles en contra'] + partido[3]
+    equipo_uno['GF'] = equipo_uno['GF'] + partido[1]
+    equipo_uno['GC'] = equipo_uno['GC'] + partido[3]
 
-    equipo_dos['goles a favor'] = equipo_dos['goles a favor'] + partido[3]
-    equipo_dos['goles en contra'] = equipo_dos['goles en contra'] + partido[1]
+    equipo_dos['GF'] = equipo_dos['GF'] + partido[3]
+    equipo_dos['GC'] = equipo_dos['GC'] + partido[1]
 
     if partido[1] > partido[3]:
         equipo_uno['puntos'] = equipo_uno['puntos'] + 3
@@ -52,12 +52,16 @@ ordenar_tabla = lambda tabla: sorted(tabla, key=lambda k: k['puntos'], reverse=T
 
 def imprimir_tabla(tabla):
     print()
-    print(f'Tabla de resultados')
+    print(f'{"Tabla de resultados".center(40, " ")}')
+    print(f'{"Equipo".center(20, " ")} {"Puntos".ljust(10, " ")} {"GF".ljust(4, " ")} {"GC".ljust(4, " ")}')
     for posicion in tabla:
-        print(posicion)
+        print(f'{posicion["nombre"].ljust(20, " ")} {str(posicion["puntos"]).ljust(10, " ")} {str(posicion["GF"]).ljust(4, " ")} {str(posicion["GC"]).ljust(4, " ")}')
 
 def iniciar_torneo():
     tabla = crear_tabla(equipos)
+    print()
+    print(f'{"Partidos Jugados".center(35, " ")}')
+    print()
     for i in range(len(equipos)):
         for j in range(i+1,len(equipos)):
             resultado_partido = jugar_partido(equipos[i], equipos[j])
